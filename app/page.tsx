218917c13a871d3a6dd0002a807438658384f1a7
jennie.shao <m65pjj@gmail.com>
2024-11-05 02:53:10 +0800

    feat: add test file for page router

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,15 @@
+import { Fragment } from "react";
+import Counter from "./counter";
+
 export const metadata = {
   title: "App Router",
 };
 
 export default function Page() {
-  return <h1>App Router</h1>;
+  return (
+    <Fragment>
+      <h1>App Router</h1>
+      <Counter />
+    </Fragment>
+  );
 }
