72d9ae902f28f659a43b24b1f8ef3ff6575cded9
jennie.shao <m65pjj@gmail.com>
2024-12-27 01:05:21 +0800

    feat(Counter): add heading

--- app/components/Counter.test.tsx
+++ app/components/Counter.test.tsx
@@ -4,6 +4,9 @@ import Counter from "./Counter";
 
 test("App Router: Works with Client Components", () => {
   render(<Counter />);
+  // console.log(prettyDOM(screen.getByRole("heading", { level: 2, name: "0" })));
+  // screen.debug(screen.getByRole("heading", { level: 2, name: "0" }));
+
   expect(screen.getByRole("heading", { level: 2, name: "0" })).toBeDefined();
   fireEvent.click(screen.getByRole("button"));
   expect(screen.getByRole("heading", { level: 2, name: "1" })).toBeDefined();
--- app/components/Counter.tsx
+++ app/components/Counter.tsx
@@ -6,6 +6,7 @@ export default function Counter() {
   const [count, setCount] = useState(0);
   return (
     <>
+      <h1 className="p-2">Counter</h1>
       <h2>{count}</h2>
       <button type="button" onClick={() => setCount(count + 1)}>
         +
